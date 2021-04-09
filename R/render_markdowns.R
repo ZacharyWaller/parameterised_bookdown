08e7e226dec83cd47e57b43e3d5e518605ce53c5
Zachary Waller <Zachary.Waller@phe.gov.uk>
2021-04-09 10:05:59 +0100

    Added fix for plots in each chapter

--- R/render_markdowns.R
+++ R/render_markdowns.R
@@ -1,5 +1,5 @@
 # name of the parameterised report rmarkdown
-rmarkdown_file <- "parameterised_report.Rmd"
+rmarkdown_file <- "parameterised_report"
 
 # the parameter we're going to pass to the report
 report_cyl <- c(4, 6, 8)
@@ -11,14 +11,24 @@ report_cyl <- c(4, 6, 8)
 markdowns <- lapply(
   report_cyl,
   function(x, filename){
+    # this folder will hold our report files - the images for plots etc.
+    output_files <- paste(filename, x, sep = "_")
 
-    rmarkdown::render(filename, params = list(cyl_num = x), run_pandoc = FALSE)
+    # name of the underlying, parameterised report to render for each chapter
+    rmd_filename <- xfun::with_ext(filename, ".Rmd")
 
-    base_filename <- xfun::sans_ext(filename)
+    # render markdown
+    # this will create a file called filename.knit.md but the report files will
+    # be put in a folder called *output_files*_files
+    rmarkdown::render(
+      rmd_filename, output_file = output_files,
+      params = list(cyl_num = x), run_pandoc = FALSE
+    )
+
     # filename.knit.md will be created by the render
-    knit_file <- xfun::with_ext(filename, ".knit.md")
+    knit_file <- xfun::with_ext(rmd_filename, ".knit.md")
     # to avoid saving over it each time, rename it
-    new_file_name <- xfun::with_ext(paste0(base_filename, "_", x), ".knit.md")
+    new_file_name <- xfun::with_ext(output_files, ".knit.md")
     file.rename(knit_file, new_file_name)
 
     # return our new file name
@@ -32,7 +42,7 @@ markdowns <- lapply(
 # do the same for the index file
 index_file <- "index.Rmd"
 rmarkdown::render(index_file, run_pandoc = FALSE)
-index_file <- xfun::with_ext(xfun::sans_ext(index_file), ".knit.md")
+index_file <- xfun::with_ext(index_file, ".knit.md")
 
 # add the index file to the list of other markdowns
 markdowns <- append(index_file, markdowns)
